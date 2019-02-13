961533efded98437854bbab678d4413c626874cc
Anushek Prasal <anushekprasal@gmail.com>
2019-02-14 00:34:43 +0530

    Remove Lineage overlays

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
